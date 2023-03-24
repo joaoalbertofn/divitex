0a464264b68bf29287634342a1ec3349350d86b5
joaoalbertofn <joaoalbertofn@gmail.com>
2023-03-24 17:00:17 +0700

    10 fiis e mobile funcionando

--- MyFirstBlazor/Data/BrapiService.cs
+++ MyFirstBlazor/Data/BrapiService.cs
@@ -2,6 +2,7 @@
 using MyFirstBlazor.Models;
 using System.Net;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace MyFirstBlazor.Data
 {
@@ -10,6 +11,37 @@ namespace MyFirstBlazor.Data
 
         private HttpClient? _client;
 
+        public Task<List<string>> GetAvaliableTickers()
+        {
+            var url = "https://brapi.dev/api/available";
+            string jsonResponse = "";
+
+            using (_client = new HttpClient())
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                request.ContentType = "application/json";
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    using (Stream stream = response.GetResponseStream())
+                    {
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+                            jsonResponse = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+
+            JObject obj = JObject.Parse(jsonResponse);
+            List<string> stocksList = obj["stocks"].ToObject<List<string>>();
+
+            stocksList.Add("10fiis");
+
+            return Task.FromResult(stocksList);
+        }
 
         public Task<Result> GetBrapiObject(string ticker)
         {
