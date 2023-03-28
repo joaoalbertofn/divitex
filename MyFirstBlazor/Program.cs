0b0cdf9d89a75ef2507f54d6b430d4994ca2dd8d
joaoalbertofn <joaoalbertofn@gmail.com>
2023-03-28 14:06:33 +0700

    cultureinfo added

--- MyFirstBlazor/Program.cs
+++ MyFirstBlazor/Program.cs
@@ -7,6 +7,11 @@ using MyFirstBlazor.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Define padrao Culture Info
+CultureInfo cultureInfo = new CultureInfo("pt-BR");
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
@@ -26,7 +31,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 
 
 app.UseHttpsRedirection();
